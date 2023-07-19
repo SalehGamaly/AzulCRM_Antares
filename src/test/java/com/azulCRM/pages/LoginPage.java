aff5b6e4d847e0aef762bb0edc5a9ce68dd3d226
SalehGamaly <123895230+SalehGamaly@users.noreply.github.com>
2023-07-18 21:13:35 -0400

    Project setup Update

---
+++ src/test/java/com/azulCRM/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package com.azulCRM.pages;
+
+
+
+import com.azulCRM.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(name="USER_LOGIN")
+    public WebElement login;
+
+
+
+    @FindBy(name="USER_PASSWORD")
+    public WebElement password;
+
+    @FindBy(className = "login-btn")
+    public WebElement loginButton;
+
+
+    public void login(String userNameStr, String passwordStr) {
+        login.sendKeys(userNameStr);
+        password.sendKeys(passwordStr);
+        loginButton.click();
+        // verification that we logged
+    }
+}
